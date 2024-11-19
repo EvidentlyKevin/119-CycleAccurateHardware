67648b299b5174e23cc39a14716743d5363c3cbf
Kevin Hack <bevinquack@duck.com>
2024-11-19 13:23:30 -0500

    Formatting

--- lib/bank_memory.cpp
+++ lib/bank_memory.cpp
@@ -25,7 +25,7 @@ void Memory::initBanks() {
     }
 }
 
-void Memory::increment(){
+void Memory::increment() {
     x++;
     y++;
     z = y - 1;
@@ -36,12 +36,6 @@ void Memory::increment(){
 void Memory::pushData(std::vector<channelM<int>>& channels, int cycle, bool debug) {
     int numBanks = MemBanks;
     int numChannels = channels.size();
-   
-    
-
-
-    
-
 
     // Vector for what data to push to each channel
     std::vector<int> data(numChannels);
@@ -50,147 +44,80 @@ void Memory::pushData(std::vector<channelM<int>>& channels, int cycle, bool debu
        
         int bankIndex = i / BANK_COLS;
 
-        // for (int j = 0; j < numBanks; ++j) {
-            int colIndex = i % BANK_COLS;
-            int rowIndex = 0;
+        int colIndex = i % BANK_COLS;
+        int rowIndex = 0;
 
-            // Max address index for pipelining logic
-            int plWindow = (cycle % (BANK_COLS * numBanks) / 3);
+        // Max address index for pipelining logic
+        int plWindow = (cycle % (BANK_COLS * numBanks) / 3);
 
-            int rowOffset = 0;
-            int delta = 0;
+        int rowOffset = 0;
+        int delta = 0;
 
-            // Adjust rowIndex to pipeline data efficiently
-            if (i < plWindow) {
-                delta = plWindow - i;
+        // Adjust rowIndex to pipeline data efficiently
+        if (i < plWindow) {
+            delta = plWindow - i;
 
-                // Compute rowOffset based on delta and numBanks
-                // This allows rowIndex to be incremented by 1 or 2
-                rowOffset = ((delta) / 3) + 1;
+            // Compute rowOffset based on delta and numBanks
+            // This allows rowIndex to be incremented by 1 or 2
+            rowOffset = ((delta) / 3) + 1;
 
-                // Ensure we don't exceed the maximum number of rows
-                if (rowIndex + rowOffset >= BANK_ROWS) {
-                    rowOffset = BANK_ROWS - 1 - rowIndex;
-                }
+            // Ensure we don't exceed the maximum number of rows
+            if (rowIndex + rowOffset >= BANK_ROWS) {
+                rowOffset = BANK_ROWS - 1 - rowIndex;
+            }
+        }
+
+        int actualRowIndex = rowIndex + rowOffset;
+
+        // Ensure indices are within bounds
+        if (colIndex < BANK_COLS && actualRowIndex < BANK_ROWS) {
+            // Get the data from the memory bank
+            if (actualRowIndex != rowIndex) {
+                // actualRowIndex = rowIndex;
+                data[delta] = MemoryBanks[bankIndex].Data[actualRowIndex][colIndex];
             }
 
-            int actualRowIndex = rowIndex + rowOffset;
+            data[i] = MemoryBanks[bankIndex].Data[rowIndex][colIndex];
 
-            // Ensure indices are within bounds
-            if (colIndex < BANK_COLS && actualRowIndex < BANK_ROWS) {
-                // Get the data from the memory bank
-                if (actualRowIndex != rowIndex) {
-                    // actualRowIndex = rowIndex;
-                    data[delta] = MemoryBanks[bankIndex].Data[actualRowIndex][colIndex];
-                    
-                }
-
-                data[i] = MemoryBanks[bankIndex].Data[rowIndex][colIndex];
-
-
-
-
-
-
-
-
-                  /*if(cycle == 2){
-                            data[0] = 2;
-                        }
-
-                        if(cycle == 5){
-                            data[1] = 2;
-                            data[0] = 3;
-                        }
-                        if(cycle == 8){
-                            data[2] = 2;
-                            data[1] = 3;
-                        }
-                        if(cycle == 11){
-                            data[3] = 2;
-                            data[2] = 3;
-                        }
-                        if(cycle == 14){
-                            data[4] = 2;
-                            data[3] = 3;
-                        }
-                        if(cycle == 17){
-                            data[5] = 2;
-                            data[4] = 3;
-                        }
-                        if(cycle == 20){
-                            data[6] = 2;
-                            data[5] = 3;
-                        }
-                        if(cycle == 23){
-                            data[7] = 2;
-                            data[6] = 3;
-                        }
-                        if(cycle == 26){
-                            data[7] = 3;
-                        }*/
-
-                        if(cycle == 2){
-                            data[0] = MemoryBanks[bankIndex].Data[rowIndex + 1][colIndex];
-
-                        }
-                        if((cycle - 2) % 3 == 0 && cycle > 2){   
-
-                                                    
-                            data[z] = MemoryBanks[bankIndex].Data[rowIndex + 2][colIndex];
-                            data[x] = MemoryBanks[bankIndex].Data[rowIndex + 1][colIndex];
-                            
-                            if (y > 7){
-                                y = 0;
-                                z = 1;
-                            }
-                            if (x > 7){
-                                x = 0;
-                            }
-                            }
-                            
-
-
-
-
-                            
-
-
-                           
-
-
-
-            
-
-
-                // Push data into the channel if it's not full
-                if (!channels[i].is_full()) {
-                    if(i == (cycle / 3) % numChannels){
-                    channels[i].push(data[i]);
-                    }
-                   // Check for data values of 3 to push into channels
-            for (int j = 0; j < numChannels; ++j) {
-                if (data[j] == 3 && !channels[j].is_full()) {
-                    channels[j].push(data[j]);
-                }
+            if(cycle == 2){
+                data[0] = MemoryBanks[bankIndex].Data[rowIndex + 1][colIndex];
             }
-                     
 
-
-                    
+            if((cycle - 2) % 3 == 0 && cycle > 2){
+                                        
+                data[z] = MemoryBanks[bankIndex].Data[rowIndex + 2][colIndex];
+                data[x] = MemoryBanks[bankIndex].Data[rowIndex + 1][colIndex];
                 
-
-                    // Debugging to ensure data is pushed
-                    
-                        std::cout << "Cycle " << cycle << ": Pushed data " << data[i]
-                                  << " from Bank " << bankIndex << ", Row " << actualRowIndex
-                                  << ", Column " << colIndex << " into Channel " << i << "\n";
-
-                                  // Increment x, y, and z only once per cycle
-    
+                if (y > 7) { // GET RID OF MAGIC NUMBER
+                    y = 0;
+                    z = 1;
+                }
+                if (x > 7) { // USE EXPRESSION OR CONSTANT
+                    x = 0;
                 }
             }
-        //}
+            
+            // Push data into the channel if it's not full
+            if (!channels[i].is_full()) {
+                if(i == (cycle / 3) % numChannels) {
+                channels[i].push(data[i]);
+                }
+                // Check for data values of 3 to push into channels
+                for (int j = 0; j < numChannels; ++j) {
+                    if (data[j] == 3 && !channels[j].is_full()) {
+                        channels[j].push(data[j]);
+                    }
+                }
+                // Debugging to ensure data is pushed
+                
+                std::cout << "Cycle " << cycle << ": Pushed data " << data[i]
+                            << " from Bank " << bankIndex << ", Row " << actualRowIndex
+                            << ", Column " << colIndex << " into Channel " << i << "\n";
+
+                // Increment x, y, and z only once per cycle
+
+            }
+        }
     }
 }
 
