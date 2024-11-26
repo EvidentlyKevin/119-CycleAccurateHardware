fb1c467550ff61dba7746973d17159d3db520db2
Kevin Hack <bevinquack@duck.com>
2024-11-26 13:03:05 -0500

    formatting

--- lib/bank_memory.cpp
+++ lib/bank_memory.cpp
@@ -91,121 +91,115 @@ void Memory::pushData(std::vector<channelM<int>>& channels, int cycle, bool debu
 
         int RowOffset2 = RowOffset - 1;
 
-            data[i] = MemoryBanks[bankIndex].Data[rowIndex][colIndex];
+        data[i] = MemoryBanks[bankIndex].Data[rowIndex][colIndex];
 
 
 
 
-            if((cycle - 3) % 6 == 0){
-                storeCycle += 3;   
-                data[0] = MemoryBanks[bankIndex].Data[rowIndex + RowOffset2][colIndex];
+        if((cycle - 3) % 6 == 0) {
+            storeCycle += 3;   
+            data[0] = MemoryBanks[bankIndex].Data[rowIndex + RowOffset2][colIndex];
+        }
+
+
+        /*if(cycle % Push_Period == 0 && cycle > storeCycle){
+            data[z] = MemoryBanks[bankIndex].Data[rowIndex + RowOffset][colIndex];
+            data[x] = MemoryBanks[bankIndex].Data[rowIndex + RowOffset2][colIndex];
+
+            if (y > numChannels - 1) { 
+                y = 0;
+                z = 7;
             }
-
-
-            /*if(cycle % Push_Period == 0 && cycle > storeCycle){
-                data[z] = MemoryBanks[bankIndex].Data[rowIndex + RowOffset][colIndex];
-                data[x] = MemoryBanks[bankIndex].Data[rowIndex + RowOffset2][colIndex];
-
-                if (y > numChannels - 1) { 
-                    y = 0;
-                    z = 7;
-                }
-                if (x > numChannels - 1) { 
-                    x = 0;
-                }
-                
-            }*/
+            if (x > numChannels - 1) { 
+                x = 0;
+            }
+            
+        }*/
             
            
          
            
-            if(cycle % 3 == 0 && cycle > 3){
-                                        
-                data[z] = MemoryBanks[bankIndex].Data[rowIndex + 2][colIndex];
-                
-                data[x] = MemoryBanks[bankIndex].Data[rowIndex + 1][colIndex];
+        if(cycle % 3 == 0 && cycle > 3){
+                                    
+            data[z] = MemoryBanks[bankIndex].Data[rowIndex + 2][colIndex];
+            
+            data[x] = MemoryBanks[bankIndex].Data[rowIndex + 1][colIndex];
 
-            if (y > BANK_ROWS - 1) { // GET RID OF MAGIC NUMBER
-                    y = 0;
-                    z = 7;
-                }
-                if (x > 7) { // USE EXPRESSION OR CONSTANT
-                    x = 0;
-                }
-
+        if (y > BANK_ROWS - 1) { // GET RID OF MAGIC NUMBER
+                y = 0;
+                z = 7;
+            }
+            if (x > 7) { // USE EXPRESSION OR CONSTANT
+                x = 0;
             }
 
+        }
+
             
-            if((cycle) % 3 == 0 && cycle > 9){
-                                        
-                data[c] = MemoryBanks[bankIndex].Data[rowIndex + 4][colIndex];
-                
-                data[a] = MemoryBanks[bankIndex].Data[rowIndex + 3][colIndex];
+        if((cycle) % 3 == 0 && cycle > 9){
+                                    
+            data[c] = MemoryBanks[bankIndex].Data[rowIndex + 4][colIndex];
+            
+            data[a] = MemoryBanks[bankIndex].Data[rowIndex + 3][colIndex];
 
-            if (b > 7) { // GET RID OF MAGIC NUMBER
-                    b = 0;
-                    c = 7;
-                }
-                if (a > 7) { // USE EXPRESSION OR CONSTANT
-                    a = 0;
-                }
-
+        if (b > 7) { // GET RID OF MAGIC NUMBER
+                b = 0;
+                c = 7;
+            }
+            if (a > 7) { // USE EXPRESSION OR CONSTANT
+                a = 0;
             }
 
-            
-            if((cycle) % 3 == 0 && cycle > 15){
-                                        
-                data[f] = MemoryBanks[bankIndex].Data[rowIndex + 6][colIndex];
-                
-                data[d] = MemoryBanks[bankIndex].Data[rowIndex + 5][colIndex];
+        }
 
-                if (e > 7) { // GET RID OF MAGIC NUMBER
-                    e = 0;
-                    f = 7;
-                }
-                if (d > 7) { // USE EXPRESSION OR CONSTANT
-                    d = 0;
-                }
+            
+        if((cycle) % 3 == 0 && cycle > 15){
+                                    
+            data[f] = MemoryBanks[bankIndex].Data[rowIndex + 6][colIndex];
+            
+            data[d] = MemoryBanks[bankIndex].Data[rowIndex + 5][colIndex];
+
+            if (e > 7) { // GET RID OF MAGIC NUMBER
+                e = 0;
+                f = 7;
             }
-
-            
-            
-            if (cycle % 3 == 0 && cycle > 21)
-            {
-                data[g] = MemoryBanks[bankIndex].Data[rowIndex + 7][colIndex];
+            if (d > 7) { // USE EXPRESSION OR CONSTANT
+                d = 0;
             }
-            
+        }
+
+        
+        
+        if (cycle % 3 == 0 && cycle > 21)
+        {
+            data[g] = MemoryBanks[bankIndex].Data[rowIndex + 7][colIndex];
+        }
+        
 
 
-            
-            
-            // Push data into the channel if it's not full
-            if (!channels[i].is_full()) {
-                if(i == (cycle / 3) % numChannels) {
+        
+        
+        // Push data into the channel if it's not full
+        if (!channels[i].is_full()) {
+            if(i == (cycle / 3) % numChannels) {
                 channels[i].push(data[i]);
-                }
-                // Check for data values to push into channels
-
-
-
-           if (data[i] >= neg_inf && data[i] <= pos_inf && data[i] != 1 && data[i] != 0) {
-            channels[i].push(data[i]);
             }
+            // Check for data values to push into channels
 
 
 
-                
+            if (data[i] >= neg_inf && data[i] <= pos_inf && data[i] != 1 && data[i] != 0) {
+                channels[i].push(data[i]);
+            }                
+            // Debugging to ensure data is pushed
+            
+            std::cout << "Cycle " << cycle << ": Pushed data " << data[i]
+                        << " from Bank " << bankIndex << ", Row " << rowIndex
+                        << ", Column " << colIndex << " into Channel " << i << "\n";
 
-                
-                // Debugging to ensure data is pushed
-                
-                std::cout << "Cycle " << cycle << ": Pushed data " << data[i]
-                            << " from Bank " << bankIndex << ", Row " << rowIndex
-                            << ", Column " << colIndex << " into Channel " << i << "\n";
+            // Increment x, y, and z only once per cycle
 
-                // Increment x, y, and z only once per cycle
-
-            }
+        }
         
     }
 }
